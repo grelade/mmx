ab76c3ee6cb543e425bb64cb56224ecc616a5037
grelade <jacekgrela@gmail.com>
2020-01-24 18:24:31 +0000

    tidying up

--- image-similarity-clustering/linker.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-imlist = os.listdir('images')
-df = pd.DataFrame({'image':imlist})
-df.index.name = 'id'
-df.to_csv('images.tsv',sep='\t')
--- image-similarity-clustering/stats.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-sorted_all = pd.read_csv('images_clusters.tsv',sep='\t')
-
-print('memes')
-print(sorted_all.drop_duplicates(subset=['cluster'])[{'cluster','image'}])
-vc = sorted_all['cluster'].value_counts()
-print('meme rank:')
-print(vc)
